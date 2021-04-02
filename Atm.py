7062689e8f69b926f4fce3c2c6d1744dafa88374
Agastheshwar2006 <72341702+Agastheshwar2006@users.noreply.github.com>
2021-04-02 14:12:01 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,51 @@
+class Atm(object):
+    def __init__(accountant, accountbalance, atmpin, atmcardnumber, atmcvv):
+
+        accountant.accountbalance=accountbalance
+        accountant.atmpin=atmpin
+        accountant.atmcardnumber=atmcardnumber
+        accountant=atmcvv=atmcvv
+
+   
+
+    def ab(accountant):
+        print("balance")
+
+    def atmp(accountant):
+        print("pin")
+
+    def atmcdn(accountant):
+        print("card.no")
+
+    def atmcw(accountant):
+        print("cvv")
+
+Agasthehwar = Atm(23000,1122,1234567890987654321,123)
+Agastheshwar.ac()
+Agastheshwar.atmpn()
+Agastheshwar.atmdn()
+Agastheshwar.atmcw()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
